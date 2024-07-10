a8fbda3e6bd6c5bf6f3f4a337f76b8ce38e396da
Ayush Kumar Singh <ayushsingh16200@gmail.com>
2024-07-10 11:19:39 +0530

    small change in home page

--- src/app/module/admin/admin.component.ts
+++ src/app/module/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { User } from '../../Class/User/user';
 import { UserService } from '../../Service/user.service';
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
   constructor(private router : Router, private userService : UserService) {}
   usr:User| null = null;
   ngOnInit(): void {
